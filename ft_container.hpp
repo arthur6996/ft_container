83f0bc399fb785ddfe89e36b572269722dd9edec
arthur6996 <arthuro6996@live.fr>
2021-03-25 11:16:21 +0100

    starting list container

--- ft_container.hpp
+++ ft_container.hpp
@@ -1,16 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   list.hpp                                           :+:      :+:    :+:   */
+/*   ft_container.hpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abourbou <abourbou@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/25 10:56:34 by abourbou          #+#    #+#             */
-/*   Updated: 2021/03/25 10:57:51 by abourbou         ###   ########lyon.fr   */
+/*   Created: 2021/03/25 10:58:26 by abourbou          #+#    #+#             */
+/*   Updated: 2021/03/25 11:11:56 by abourbou         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_LIST_HPP
-# define FT_LIST_HPP
+#ifndef FT_CONTAINER_HPP
+# define FT_CONTAINER_HPP
+
+#include <list>
+
+namespace ft
+{
+	class list;
+}
 
 #endif
